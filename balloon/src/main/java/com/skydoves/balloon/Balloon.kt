0d8e1c06027695b6c9644c9a2e48c5540d2188f6
skydoves <skydoves@naver.com>
2021-06-29 21:22:37 +0900

    Move post runner inside of the if conditional and check isAttachedToWindow

--- balloon/src/main/java/com/skydoves/balloon/Balloon.kt
+++ balloon/src/main/java/com/skydoves/balloon/Balloon.kt
@@ -554,18 +554,20 @@ class Balloon(
    */
   @MainThread
   private inline fun show(anchor: View, crossinline block: () -> Unit) {
-    anchor.post {
-      if (!isShowing &&
-        // If the balloon is already destroyed depending on the lifecycle,
-        // We should not allow showing the popupWindow, it's related to `relay()` method. (#46)
-        !destroyed &&
-        // We should check the current Activity is running.
-        // If the Activity is finishing, we can't attach the popupWindow to the Activity's window. (#92)
-        !context.isFinishing() &&
-        // We should check the contentView is already attached to the decorView or backgroundView in the popupWindow.
-        // Sometimes there is a concurrency issue between show and dismiss the popupWindow. (#149)
-        bodyWindow.contentView.parent == null
-      ) {
+    if (!isShowing &&
+      // If the balloon is already destroyed depending on the lifecycle,
+      // We should not allow showing the popupWindow, it's related to `relay()` method. (#46)
+      !destroyed &&
+      // We should check the current Activity is running.
+      // If the Activity is finishing, we can't attach the popupWindow to the Activity's window. (#92)
+      !context.isFinishing() &&
+      // We should check the contentView is already attached to the decorView or backgroundView in the popupWindow.
+      // Sometimes there is a concurrency issue between show and dismiss the popupWindow. (#149)
+      bodyWindow.contentView.parent == null &&
+      // we should check the anchor view is attached to the parent's window.
+      ViewCompat.isAttachedToWindow(anchor)
+    ) {
+      anchor.post {
         this.builder.preferenceName?.let {
           if (balloonPersistence.shouldShowUp(it, builder.showTimes)) {
             balloonPersistence.putIncrementedCounts(it)
@@ -599,9 +601,9 @@ class Balloon(
         applyBalloonAnimation()
         startBalloonHighlightAnimation()
         block()
-      } else if (builder.dismissWhenShowAgain) {
-        dismiss()
       }
+    } else if (builder.dismissWhenShowAgain) {
+      dismiss()
     }
   }
 
