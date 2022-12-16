6753308f368ddc38f8562be8a4a1b654c45836e8
Matt Clarke <kiwiandroiddev@gmail.com>
2022-12-16 14:25:43 +1300

    Fix - avoids setting no-op click listener on balloon to improve Talkback hint

--- balloon/src/main/kotlin/com/skydoves/balloon/Balloon.kt
+++ balloon/src/main/kotlin/com/skydoves/balloon/Balloon.kt
@@ -1215,9 +1215,11 @@ public class Balloon private constructor(
 
   /** sets a [OnBalloonClickListener] to the popup. */
   public fun setOnBalloonClickListener(onBalloonClickListener: OnBalloonClickListener?) {
-    this.binding.balloonWrapper.setOnClickListener {
-      onBalloonClickListener?.onBalloonClick(it)
-      if (builder.dismissWhenClicked) dismiss()
+    if (onBalloonClickListener != null || builder.dismissWhenClicked) {
+      this.binding.balloonWrapper.setOnClickListener {
+        onBalloonClickListener?.onBalloonClick(it)
+        if (builder.dismissWhenClicked) dismiss()
+      }
     }
   }
 
