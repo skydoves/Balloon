40b30c6e267b1e260e47364b3db4713c41b99062
skydoves <skydoves@naver.com>
2023-08-26 18:04:43 +0900

    Refactor BaselineProfileGenerator

--- benchmark/src/main/kotlin/com/skydoves/balloon/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/kotlin/com/skydoves/balloon/benchmark/BaselineProfileGenerator.kt
@@ -16,18 +16,22 @@
 
 package com.skydoves.balloon.benchmark
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import org.junit.Rule
 import org.junit.Test
 
+@RequiresApi(Build.VERSION_CODES.P)
 class BaselineProfileGenerator {
   @get:Rule
   val baselineProfileRule = BaselineProfileRule()
 
   @Test
   fun startup() =
-    baselineProfileRule.collectBaselineProfile(
+    baselineProfileRule.collect(
       packageName = "com.skydoves.balloon.benchmark.app",
+      includeInStartupProfile = true
     ) {
       pressHome()
       // This block defines the app's critical user journey. Here we are interested in
