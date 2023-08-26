56bdda1e4f808fa7dab8f3b41b50e3e1c3b1ede5
skydoves <skydoves@naver.com>
2023-08-26 20:30:47 +0900

    Update jvm target to 17 for benchmark module

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -25,12 +25,12 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
 
   kotlinOptions {
-    jvmTarget = libs.versions.jvmTarget.get()
+    jvmTarget = "17"
   }
 
   buildTypes {
