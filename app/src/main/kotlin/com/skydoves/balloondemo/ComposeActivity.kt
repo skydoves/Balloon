383e839f8612387502450e6d13bace149699827e
skydoves <skydoves@naver.com>
2023-12-27 18:05:37 +0900

    Implement a sample for repositioning with LazyColumn

--- app/src/main/kotlin/com/skydoves/balloondemo/ComposeActivity.kt
+++ app/src/main/kotlin/com/skydoves/balloondemo/ComposeActivity.kt
@@ -17,15 +17,15 @@
 package com.skydoves.balloondemo
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
@@ -35,9 +35,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.skydoves.balloon.ArrowPositionRules
+import com.skydoves.balloon.ArrowOrientation
 import com.skydoves.balloon.BalloonAnimation
 import com.skydoves.balloon.BalloonHighlightAnimation
 import com.skydoves.balloon.BalloonSizeSpec
@@ -56,17 +59,15 @@ class ComposeActivity : ComponentActivity() {
         setArrowSize(10)
         setWidth(BalloonSizeSpec.WRAP)
         setHeight(BalloonSizeSpec.WRAP)
-        setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
-        setArrowPosition(0.5f)
+        setArrowPosition(0.2f)
+        setArrowOrientation(ArrowOrientation.BOTTOM)
         setPadding(12)
         setMarginHorizontal(12)
         setTextSize(15f)
         setCornerRadius(8f)
         setBackgroundColorResource(R.color.skyBlue)
         setBalloonAnimation(BalloonAnimation.ELASTIC)
-        setIsVisibleOverlay(true)
-        setOverlayColorResource(R.color.overlay)
-        setOverlayPaddingResource(R.dimen.editBalloonOverlayPadding)
+        setDismissWhenTouchOutside(false)
         setBalloonHighlightAnimation(BalloonHighlightAnimation.SHAKE)
         setOverlayShape(
           BalloonOverlayRoundRect(
@@ -77,99 +78,47 @@ class ComposeActivity : ComponentActivity() {
         setDismissWhenClicked(true)
       }
 
-      var balloonWindow1: BalloonWindow? by remember { mutableStateOf(null) }
-      var balloonWindow2: BalloonWindow? by remember { mutableStateOf(null) }
-      var balloonWindow3: BalloonWindow? by remember { mutableStateOf(null) }
-
       Box(modifier = Modifier.fillMaxSize()) {
-        Balloon(
-          modifier = Modifier
-            .padding(20.dp)
-            .align(Alignment.Center),
-          builder = builder,
-          onComposedAnchor = {
-            balloonWindow1?.showAlignTop()
-          },
-          onBalloonWindowInitialized = {
-            balloonWindow1 = it
-          },
-          balloonContent = {
-            Text(
-              text = "Now you can edit your profile1 profile2 profile3 profile4",
-              textAlign = TextAlign.Center,
-              color = Color.White,
-            )
-          },
-        ) {
-          Button(
-            modifier = Modifier.size(160.dp, 60.dp),
-            onClick = { balloonWindow1?.showAlignTop() },
-          ) {
-            Text(text = "showAlignTop")
-          }
-        }
 
-        Balloon(
-          modifier = Modifier
-            .padding(20.dp)
-            .align(Alignment.TopStart),
-          builder = builder,
-          onBalloonWindowInitialized = {
-            balloonWindow2 = it
-          },
-          balloonContent = {
-            Text(
-              text = "Now you can edit your profile!",
-              textAlign = TextAlign.Center,
-              color = Color.White,
-            )
-          },
-        ) {
-          Button(
-            modifier = Modifier.size(160.dp, 60.dp),
-            onClick = { balloonWindow2?.showAlignTop() },
-          ) {
-            Text(text = "wrap balloon")
-          }
-        }
+        val items = List(20) { it.toString() }
+        val view = LocalView.current
 
-        Balloon(
-          modifier = Modifier
-            .padding(20.dp)
-            .align(Alignment.TopEnd),
-          builder = builder,
-          onBalloonWindowInitialized = {
-            balloonWindow3 = it
-          },
-          balloonContent = {
-            Box(modifier = Modifier.fillMaxWidth()) {
-              Box(
+        LazyColumn {
+          items(items = items, key = { it }) {
+            var balloonWindow1: BalloonWindow? by remember { mutableStateOf(null) }
+            var anchorView: View? by remember { mutableStateOf(null) }
+
+            Balloon(
+              modifier = Modifier
+                .padding(20.dp)
+                .align(Alignment.Center)
+                .onGloballyPositioned {
+                  val position = it.positionInWindow()
+                  if (balloonWindow1?.balloon?.isShowing == true) {
+                    val calculatedY =
+                      position.y.toInt() - (balloonWindow1!!.getMeasuredHeight() + anchorView!!.measuredHeight / 2)
+                    balloonWindow1!!.balloon.update(view, 0, calculatedY)
+                  }
+                },
+              builder = builder,
+              onComposedAnchor = { anchorView = it },
+              onBalloonWindowInitialized = { balloonWindow1 = it },
+              balloonContent = {
+                Text(
+                  text = "Now you can edit your profile1 profile2 profile3 profile4",
+                  textAlign = TextAlign.Center,
+                  color = Color.White,
+                )
+              },
+            ) {
+              Button(
                 modifier = Modifier
-                  .size(50.dp)
-                  .align(Alignment.CenterStart)
-                  .background(Color.Blue),
-              )
-              Box(
-                modifier = Modifier
-                  .size(50.dp)
-                  .align(Alignment.Center)
-                  .background(Color.Blue),
-              )
-              Box(
-                modifier = Modifier
-                  .size(50.dp)
-                  .border(2.dp, Color.Red)
-                  .align(Alignment.CenterEnd)
-                  .background(Color.Blue),
-              )
+                  .size(160.dp, 60.dp),
+                onClick = { balloonWindow1?.showAlignTop() },
+              ) {
+                Text(text = "showAlignTop")
+              }
             }
-          },
-        ) {
-          Button(
-            modifier = Modifier.size(160.dp, 60.dp),
-            onClick = { balloonWindow3?.showAlignBottom() },
-          ) {
-            Text(text = "alignments")
           }
         }
       }
