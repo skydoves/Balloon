579ad30760fb04cf9ccc190938c1793791335526
Tobi <miketobi73@gmail.com>
2025-07-06 12:54:17 +0100

    mark BalloonStroke as data class

--- balloon/src/main/kotlin/com/skydoves/balloon/BalloonStroke.kt
+++ balloon/src/main/kotlin/com/skydoves/balloon/BalloonStroke.kt
@@ -3,7 +3,7 @@ package com.skydoves.balloon
 import androidx.annotation.ColorInt
 import com.skydoves.balloon.annotations.Dp
 
-public class BalloonStroke(
+public data class BalloonStroke(
   @ColorInt public val color: Int,
   @Dp public val thickness: Float = 1f
 )
