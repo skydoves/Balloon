ef51acdcb34576abcda92dbfa14d988bf5e1d193
skydoves <skydoves@naver.com>
2026-03-10 20:26:33 +0900

    Prepare for release 1.7.5

--- buildSrc/src/main/kotlin/com/skydoves/balloon/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/balloon/Configuration.kt
@@ -23,9 +23,9 @@ object Configuration {
   const val minSdkBenchmark = 23
   const val majorVersion = 1
   const val minorVersion = 7
-  const val patchVersion = 4
+  const val patchVersion = 5
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 73
+  const val versionCode = 74
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
