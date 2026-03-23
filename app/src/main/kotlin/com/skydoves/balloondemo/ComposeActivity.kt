2e04cb14ee12ed97dd52fade4d970510d755b12a
chinmay <chinmaydash11@gmail.com>
2026-03-23 16:40:06 +0530

    added top and bottom bar insets.

--- app/src/main/kotlin/com/skydoves/balloondemo/ComposeActivity.kt
+++ app/src/main/kotlin/com/skydoves/balloondemo/ComposeActivity.kt
@@ -29,11 +29,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -53,8 +57,6 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -201,7 +203,12 @@ private fun TopAppBar(onToast: (String) -> Unit) {
     modifier = Modifier
       .fillMaxWidth()
       .background(Pink)
-      .padding(horizontal = 8.dp, vertical = 12.dp),
+      .padding(
+        top = 12.dp + WindowInsets.statusBars.asPaddingValues().calculateTopPadding(),
+        bottom = 12.dp,
+        start = 8.dp,
+        end = 8.dp,
+      ),
     horizontalArrangement = Arrangement.SpaceBetween,
     verticalAlignment = Alignment.CenterVertically,
   ) {
@@ -1115,7 +1122,12 @@ private fun BottomNavigation(onToast: (String) -> Unit) {
     modifier = Modifier
       .fillMaxWidth()
       .background(Pink)
-      .padding(vertical = 8.dp),
+      .padding(
+        top = 8.dp,
+        bottom = 8.dp + WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding(),
+        start = 8.dp,
+        end = 8.dp,
+      ),
     horizontalArrangement = Arrangement.SpaceEvenly,
   ) {
     BottomNavItem(
